bc9916bf740e853f9354427fbeb6cc850624f8cf
Hazman Hassan <hazmanhassan912@gmail.com>
2026-06-18 12:51:27 +0800

    trying qr code scanner

--- public/app.js
+++ public/app.js
@@ -589,6 +589,7 @@ function stopQRScanner() {
 function onQrScanSuccess(decodedText, decodedResult) {
   // When a QR is scanned, the decodedText is the ID of the card
   console.log(`QR Scanned successfully: ${decodedText}`);
+  stopQRScanner();
 
   // Play a brief beep alert or update status
   scannerStatus.textContent = 'Scanned!';
